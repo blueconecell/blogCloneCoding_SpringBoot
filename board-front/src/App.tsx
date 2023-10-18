e39252099ec13e9d7b17ec59344631fb11850593
JeongYeonKim <95961799+blueconecell@users.noreply.github.com>
2023-10-18 23:41:15 +0900

    3. 컴포넌트 생성(input box-a)

--- board-front/src/App.tsx
+++ board-front/src/App.tsx
@@ -6,13 +6,12 @@ import Top3Item from 'components/Top3Item';
 import top3BoardListMock from 'mocks/top-3-board-list.mock';
 import CommentItem from 'components/CommentItem';
 import FavoriteItem from 'components/FavoriteItem';
+import InputBox from 'components/InputBox';
 
 function App() {
   return (
     <>
-    <div style={{padding:'0 20px', display: 'flex', flexDirection:'column',columnGap:'30px',rowGap:'20px'}}>
-      {favoriteListMock.map(favoriteListItem => <FavoriteItem favoriteListItem={favoriteListItem}/>)}
-    </div>
+      <InputBox/>
     </>
   );
 }
