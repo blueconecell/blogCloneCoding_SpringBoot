7330a8e546c81e8d14af5f6f91b4b6e2caf98d52
JeongYeonKim <95961799+blueconecell@users.noreply.github.com>
2023-11-03 21:56:33 +0900

    3. Header 레이아웃 작성 - c (완)

--- board-front/src/layouts/Header/index.tsx
+++ board-front/src/layouts/Header/index.tsx
@@ -1,7 +1,7 @@
 import React, { ChangeEvent, useRef, useState,KeyboardEvent, useEffect } from 'react'
 import './style.css'
-import { useNavigate, useParams } from 'react-router-dom';
-import { AUTH_PATH, MAIN_PATH, SEARCH_PATH, USER_PATH } from 'constant';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import { AUTH_PATH, BOARD_DETAIL_PATH, BOARD_PATH, BOARD_UPDATE_PATH, BOARD_WRITE_PATH, MAIN_PATH, SEARCH_PATH, USER_PATH } from 'constant';
 import { useCookies } from 'react-cookie';
 import useLoginUserStore from 'stores/login-user.store';
 import { useBoardStore } from 'stores';
@@ -11,11 +11,29 @@ export default function Header() {
 
   // state: 로그인 유저 상태
   const{loginUser, resetLoginUser} = useLoginUserStore();
-
+  // state: path 상태
+  const {pathname} = useLocation();
   // state: cookie 상태
   const [cookies, setCookie] = useCookies();
   // state: 로그인 상태
   const [isLogin, setLogin] = useState<boolean>(false);
+  // state: 인증 페이지 상태
+  const [isAuthPage, setAuthPage] = useState<boolean>(false);
+  // state: 메인 페이지 상태
+  const [isMainPage, setMainPage] = useState<boolean>(false);
+  // state: 검색 페이지 상태
+  const [isSearchPage, setSearchPage] = useState<boolean>(false);
+  // state: 게시물 상세 페이지 상태
+  const [isBoardDetailPage, setBoardDetailPage] = useState<boolean>(false);
+  // state: 게시물 작성 페이지 상태
+  const [isBoardWritePage, setBoardWritePage] = useState<boolean>(false);
+  // state: 게시물 수정 페이지 상태
+  const [isBoardUpdatePage, setBoardUpdatePage] = useState<boolean>(false);
+  // state: 유저 페이지 상태
+  const [isUserPage, setUserPage] = useState<boolean>(false);
+
+
+
   
 
   // function : 네비게이트 함수
@@ -151,6 +169,23 @@ export default function Header() {
 
   }
 
+  // effect: path가 변경될 때마다 실행될 함수
+  useEffect(() => {
+    const isAuthPage = pathname.startsWith(AUTH_PATH());
+    setAuthPage(isAuthPage);
+    const isMainPage = pathname === MAIN_PATH();
+    setMainPage(isMainPage);
+    const isSearchPage = pathname.startsWith(SEARCH_PATH(''));
+    setSearchPage(isSearchPage);
+    const isBoardDetailPage = pathname.startsWith(BOARD_PATH() + '/' +  BOARD_DETAIL_PATH(''));
+    setBoardDetailPage(isBoardDetailPage);
+    const isBoardWritePage = pathname.startsWith(BOARD_PATH() + '/' +  BOARD_WRITE_PATH());
+    setBoardWritePage(isBoardWritePage);
+    const isBoardUpdatePage = pathname.startsWith(BOARD_PATH() + '/' +  BOARD_UPDATE_PATH(''));
+    setBoardUpdatePage(isBoardUpdatePage);
+    const isUserPage = pathname.startsWith(USER_PATH(''));
+    setUserPage(isUserPage);
+  })
 
 
 
@@ -167,9 +202,10 @@ export default function Header() {
           <div className='header-logo'>{'Hoons Board'}</div>
         </div>
       <div className='header-right-box'>
-        <SearchButton/>
-        <MyPageButton/>
-        <UploadButton/>
+
+        {(isAuthPage || isMainPage || isSearchPage || isBoardDetailPage) && <SearchButton/>}
+        {(isMainPage || isSearchPage || isBoardDetailPage || isUserPage) && <MyPageButton/>}
+        {(isBoardWritePage || isBoardUpdatePage) &&<UploadButton/>}
       </div>
     </div>
   </div>
