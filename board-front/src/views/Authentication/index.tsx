7865aa6064624fad1962355a39a3c9105d52cf18
JeongYeonKim <95961799+blueconecell@users.noreply.github.com>
2023-11-30 20:16:58 +0900

    4. Auth 페이지 회원가입 API 연동
    1. 로그인 회원가입 둘다 안되는 버그
    2. 회원가입 카드에서 오류 메시지 재대로 적용 안되는 버그

--- board-front/src/views/Authentication/index.tsx
+++ board-front/src/views/Authentication/index.tsx
@@ -1,9 +1,9 @@
-import { useState,KeyboardEvent,useRef,ChangeEvent} from 'react';
+import { useState,KeyboardEvent,useRef,ChangeEvent, useEffect} from 'react';
 import './style.css';
 import InputBox from 'components/InputBox';
-import { signInRequest } from 'apis';
-import { SignInRequestDto } from 'apis/request/auth';
-import { SignInResponseDto } from 'apis/response/auth';
+import { signInRequest, signUpRequest } from 'apis';
+import { SignInRequestDto, SignUpRequestDto } from 'apis/request/auth';
+import { SignInResponseDto, SignUpResponseDto } from 'apis/response/auth';
 import { ResponseDto } from 'apis/response';
 import { useCookies } from 'react-cookie';
 import { MAIN_PATH } from 'constant';
@@ -165,7 +165,7 @@ export default function Authentication(){
 
 
         // state: 페이지 번호 상태
-        const [page, setPage] = useState<1 | 2>(2);
+        const [page, setPage] = useState<1 | 2>(1);
         // state: 이메일 상태
         const [email, setEmail] = useState<string>('');
         // state: 패스워드 상태
@@ -221,8 +221,34 @@ export default function Authentication(){
         const [passwordButtonIcon, setPasswordButtonIcon] = useState<'eye-light-off-icon'|'eye-light-on-icon'>('eye-light-off-icon');
         // state: 패스워드 확인 버튼 아이콘 상태
         const [passwordCheckButtonIcon, setPasswordCheckButtonIcon] = useState<'eye-light-off-icon'|'eye-light-on-icon'>('eye-light-off-icon');
+        
         // function: 다음 주소 검색 팝업 오픈 함수
         const open  = useDaumPostcodePopup();
+        // function: sign up response 처리 함수
+        const signUpResponse = (responseBody: SignUpResponseDto | ResponseDto | null) => {
+            if (!responseBody){
+                alert('네트워크 이상입니다');
+                return;
+            }
+            const {code} = responseBody;
+            if(code === 'DE'){
+                setEmailError(true);
+                setEmailErrorMessage('중복되는 이메일 주소입니다.');
+            }
+            if (code === 'DN'){
+                setNicknameError(true);
+                setNicknameErrorMessage('중복되는 닉네임입니다.');
+            }
+            if (code === 'DT'){
+                setTelNumberError(true);
+                setTelNumberErrorMessage('중복되는 핸드폰 번호입니다.');
+            }
+            if (code === 'VF') alert('모든 값을 입력하세요');
+            if (code === 'DBE') alert('데이터베이스 오류입니다.');
+            if (code === 'SU') return;
+            setView('sign-in');
+        }
+
         // event handler: 이메일 변경 이벤트 처리
         const onEmailChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
             const { value } = event.target;
@@ -282,7 +308,7 @@ export default function Authentication(){
         const onPasswordButtonClickHandler = () =>{
             if(passwordButtonIcon === 'eye-light-off-icon'){
                 setPasswordButtonIcon('eye-light-on-icon');
-                setPasswordType('password');
+                setPasswordType('text');
             }
             else{
                 setPasswordButtonIcon('eye-light-off-icon');
@@ -291,9 +317,9 @@ export default function Authentication(){
         }
         //event handler: 패스워드 확인 버튼 클릭 이벤트 처리 
         const onPasswordCheckButtonClickHandler = () =>{
-            if(passwordButtonIcon === 'eye-light-off-icon'){
+            if(passwordCheckButtonIcon === 'eye-light-off-icon'){
                 setPasswordCheckButtonIcon('eye-light-on-icon');
-                setPasswordCheckType('password');
+                setPasswordCheckType('text');
             }
             else{
                 setPasswordCheckButtonIcon('eye-light-off-icon');
@@ -309,6 +335,7 @@ export default function Authentication(){
         const onNextButtonClickHandler = () =>{
             const emailPattern = /^[a-zA-Z0-9]*@([-.]?[a-zA-Z0-9])*\.[a-zA-Z]{2,4}$/;
             const isEmailPattern = emailPattern.test(email);
+            
             if (!isEmailPattern){
                 setEmailError(true);
                 setEmailErrorMessage('이메일 주소 포멧이 맞지 않습니다.');
@@ -316,19 +343,78 @@ export default function Authentication(){
             const isCheckedPassword = password.trim().length >=8;
             if (!isCheckedPassword){
                 setPasswordError(true);
-                setPasswordErrorMessage('비밀번호는 8자 이상 입력해주세요');
+                setPasswordErrorMessage('1 비밀번호는 8자 이상 입력11111일해주세요');
             }
             const isEqualPassword = password === passwordCheck;
             if(!isEqualPassword){
                 setPasswordCheckError(true);
                 setPasswordCheckErrorMessage('비밀번호가 일치하지 않습니다.');
             }
+            
+            console.log("isEmailPattern >",isEmailPattern);
+            console.log("emailErrorMessage :",emailErrorMessage);
+            
+              
+            console.log("isCheckedPassword >",isCheckedPassword);
+            console.log("passwordErrorMessage :",passwordErrorMessage);
+            console.log("isEqualPassword >",isEqualPassword);
+            console.log("passwordCheckErrorMessage :",passwordCheckErrorMessage);
+            
             if (!isEmailPattern || !isCheckedPassword || !isEqualPassword) return;
             setPage(2);
         }
+        // effect: 페이지가 변경될 때마다 실행될 함수
+        useEffect(() => {
+            console.log("emailErrorMessage in useEffect:", emailErrorMessage);
+          }, [emailErrorMessage]);
         // event handler: 회원가입 버튼 클릭 이벤트 처리
         const onSignUpButtonClickHandler = () =>{
             alert('회원가입 버튼!');
+            const emailPattern = /^[a-zA-Z0-9]*@([-.]?[a-zA-Z0-9])*\.[a-zA-Z]{2,4}$/;
+            const isEmailPattern = emailPattern.test(email);
+            if (!isEmailPattern){
+                setEmailError(true);
+                setEmailErrorMessage('이메일 주소 포멧이 맞지 않습니다.');
+            }
+            const isCheckedPassword = password.trim().length >=8;
+            if (!isCheckedPassword){
+                setPasswordError(true);
+                setPasswordErrorMessage('비밀번호는 8자 이상 입력22222이해주세요');
+            }
+            const isEqualPassword = password === passwordCheck;
+            if(!isEqualPassword){
+                setPasswordCheckError(true);
+                setPasswordCheckErrorMessage('비밀번호가 일치하지 않습니다.');
+            }
+            if (!isEmailPattern || !isCheckedPassword || !isEqualPassword){
+                setPage(1);
+                return;
+            }
+            const hasNickname = nickname.length !==0;
+            if (!hasNickname){
+                setNicknameError(true);
+                setNicknameErrorMessage('닉네임을 입력해주세요.');
+            }
+            const telNumberPatter = /^[0-9]{11,13}$/;
+            const isTelNumberPattern = telNumberPatter.test(telNumber);
+            if(!isTelNumberPattern){
+                setTelNumberError(true);
+                setTelNumberErrorMessage('숫자만 입력해주세요.');
+            }
+            const hasAddress = address.trim().length > 0;
+            if(!hasAddress){
+                setAddressError(true);
+                setAddressErrorMessage('주소를 선택해주세요');
+            }
+            if (!agreedPersonal) setAgreedPersonalError(true);
+
+            if (!hasNickname || !isTelNumberPattern || !agreedPersonal) return;
+
+            const requestBody: SignUpRequestDto = {
+                email, password, nickname, telNumber, address, addressDetail, agreedPersonal
+            }
+            signUpRequest(requestBody).then(signUpResponse);
+
         }
 
         //event handler: 로그인 링크 클릭 이벤트 처리
@@ -352,9 +438,9 @@ export default function Authentication(){
         //event handler: 패스워드 확인 키 다운 이벤트 처리
         const onPasswordCheckKeyDownHandler = (event: KeyboardEvent<HTMLInputElement>)=>{
             if (event.key !== 'Enter') return;
-            if (!nicknameRef.current) return;
+            
             onNextButtonClickHandler();
-            nicknameRef.current.focus();
+            
         }
         //event handler: 닉네임 키 다운 이벤트 처리
         const onNicknameKeyDownHandler = (event: KeyboardEvent<HTMLInputElement>) => {
@@ -382,9 +468,16 @@ export default function Authentication(){
         const onComplete = (data: Address) =>{
             const {address} = data;
             setAddress(address);
+            setAddressError(false);
+            setAddressErrorMessage('');
             if (!addressDetailRef.current) return;
             addressDetailRef.current.focus();
         };
+
+        useEffect(() => {if (page===2){
+            if (!nicknameRef.current) return;
+            nicknameRef.current.focus();
+        }},[page])
 
         // render: sign up card rendering
         return(
@@ -414,7 +507,7 @@ export default function Authentication(){
                     </div>
                 <div className='auth-card-bottom'>
                     {page===1 && (
-                        <div className='black-large-full-bottom' onClick={onNextButtonClickHandler}>{'다음 단계'}</div>
+                        <div className='black-large-full-button' onClick={onNextButtonClickHandler}>{'다음 단계'}</div>
                     )}
                     {page===2 && (
                         <>
